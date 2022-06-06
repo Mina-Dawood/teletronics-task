47948883fd259fa5010665df946011519ab6b272
Mina Dawoud <mina.dawood.s@gmail.com>
2022-06-06 22:55:00 +0400

    add login scenario, authentication, JWT, add global styles and create loader component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService, PAGES_CONFIG } from '@app/shared';
 
 @Component({
   selector: 'teletronics-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  /**
+   * Method for simulate login and check if logged in successfully then navigate to items page
+   */
+  getStarted(): void {
+    this.authenticationService.login().subscribe((loggedIn) => {
+      if (loggedIn) {
+        this.router.navigate([PAGES_CONFIG.cryptoCurrency.route]);
+      }
+    });
   }
-
 }
---
+++ src/app/shared/guards/authentication.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { PAGES_CONFIG } from '@app/shared';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!!localStorage.getItem('token')) {
+      return true;
+    }
+
+    this.router.navigate([PAGES_CONFIG.home.route]);
+    return false;
+  }
+}
